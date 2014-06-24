64b24df583b8fda3bddd99f31fe6258c4c29805f
eslaweski <eslaweski@eslaweski.etc.etcusa.local>
2014-06-24 12:09:57 -0400

    Added the "License Agreement"

--- src/gyroreader/GyroReader.java
+++ src/gyroreader/GyroReader.java
@@ -5,7 +5,6 @@
  * you can do whatever you want with this stuff. If we meet some day, and you 
  * think this stuff is worth it, you can buy me a beer in return. Eric Slaweski
  * ----------------------------------------------------------------------------
- * 
  */
 
 package gyroreader;
